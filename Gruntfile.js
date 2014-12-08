34991c69f3e83eaae0a8d32ec36f75b753f0f29f
emnetag <emnetg@uw.edu>
2014-12-07 16:35:32 -0800

    completed advanced grunt challenge

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js'],
+            options: {
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'js/<%= pkg.name %>.min.js': ['js/*.js', '/*.js']
+                }
+            }
+        },
+        cssmin: {
+            dist: {
+                files: {
+                    'css/<%= pkg.name %>.min.css': ['css/main.css']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+};
